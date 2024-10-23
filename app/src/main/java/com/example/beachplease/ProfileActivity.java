1179d71a45b9c2bd4b530fb2364280d85765c33c
Renee Pan <reneepan@usc.edu>
2024-10-23 14:07:54 -0700

    backend class foundation

--- app/src/main/java/com/example/beachplease/ProfileActivity.java
+++ app/src/main/java/com/example/beachplease/ProfileActivity.java
@@ -1,4 +1,39 @@
 package com.example.beachplease;
 
-public class ProfileActivity {
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProfileActivity extends AppCompatActivity {
+    private User user;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        Intent intent = getIntent();
+    }
+
+    private void getUserInfo() {
+
+    }
+
+    private void listReviews() {
+
+    }
+
+    public void mapClick(android.view.View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    //should this be an option or should it refresh the profile?
+    public void profileClick(android.view.View view) {
+        Intent intent = new Intent(this, ProfileActivity.class);
+        startActivity(intent);
+        finish();
+    }
 }
