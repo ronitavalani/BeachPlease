686533502386912e069f7447dffdd8236c07e9f1
Renee Pan <reneepan@usc.edu>
2024-10-31 13:12:44 -0700

    beach to database

--- app/src/main/java/com/example/beachplease/Beach.java
+++ app/src/main/java/com/example/beachplease/Beach.java
@@ -1,47 +1,93 @@
 package com.example.beachplease;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Beach {
     private String name;
-    private String location;
+    private Double longitude;
+    private Double latitude;
     private String hours;
-    private Float averageRating;
-    private List<Review> reviews;
+    private Double averageRating;
+    private String picture;
+    private List<String> reviews;
+    private List<String> tags;
     private WeatherService forecast;
+    private String blurb;
 
-    public Beach() {
-        //connect with the database somehow
+    public Beach(String name, Double longitude, Double latitude, String hours, Double averageRating, String picture, String blurb) {
+        this.name = name;
+        this.longitude = longitude;
+        this.latitude = latitude;
+        this.hours = hours;
+        this.averageRating = averageRating;
+        this.picture = picture;
+        this.blurb = blurb;
+
+        this.reviews = new ArrayList<>();
+        this.tags = new ArrayList<>();
     }
 
     public String getName() {
         return name;
     }
 
-    public String getLocation() {
-        return location;
+    public Double getLongitude() {
+        return longitude;
     }
+
+    public Double getLatitude() { return latitude; }
 
     public String getHours() {
         return hours;
     }
 
-    public Float getAvgRating() {
+    public Double getAvgRating() {
         return averageRating;
     }
 
-    public List<Review> getReviews() {
+    public List<String> getReviews() {
         return reviews;
     }
+
+    public List<String> getTags() {return tags;}
 
     public WeatherService getForecast() {
         return forecast;
     }
 
+    public String getBlurb() { return blurb; }
+
+    public String getPicture() { return picture; }
+
+    public void addReview(String reviewId) {
+        reviews.add(reviewId);
+    }
+
+    public void addTag(String tag) {
+        tags.add(tag);
+    }
+
     public void updateAvgRating(Double rating) {
         int reviewNum = reviews.size();
-        //take this new review rating and update the average
+        double newAvg = ((averageRating * reviewNum) + rating)/(reviewNum + 1);
+        averageRating = newAvg;
+    }
 
+    public void addBeachToFirebase() {
+        DatabaseReference database = FirebaseDatabase.getInstance().getReference("beaches");
+        String key = name;
+        database.child(key).setValue(this).addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                System.out.println("successful!");
+            }
+            else {
+                System.out.println("Error: " + task.getException());
+            }
+        });
     }
 
 }
--- app/src/main/java/com/example/beachplease/BeachActivity.java
+++ app/src/main/java/com/example/beachplease/BeachActivity.java
@@ -31,7 +31,7 @@ public class BeachActivity extends AppCompatActivity {
     public void writeReview (String beachName, Double rating, Date date, User author, String comment) {
         Review newReview = new Review(beachName, rating, date, author, comment);
         beach.updateAvgRating(rating);
-        beach.getReviews().add(newReview);
+        //beach.getReviews().add(newReview);
     }
 
     //should this even be an option or should it just refresh the map?
