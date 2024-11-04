0ae085f79386e5e65b28a14cbc0d4cef9bb14cf3
Ronit Avalani <avalani@usc.edu>
2024-11-04 13:37:07 -0800

    Review Displayed on Profile and Beach

--- app/src/main/java/com/example/beachplease/Review.java
+++ app/src/main/java/com/example/beachplease/Review.java
@@ -8,44 +8,70 @@ public class Review {
     private String beachName;
     private Double rating;
     private Date date;
-    private User author;
+    private String author;
     private String comment;
     private List<String> tags;
 
-    public Review(String beachName, Double rating, Date date, User author, String comments, ArrayList<String> tags) {
+    // No-argument constructor required for Firebase
+    public Review() {
+    }
+
+    // Full constructor
+    public Review(String beachName, Double rating, Date date, String author, String comment, ArrayList<String> tags) {
         this.beachName = beachName;
         this.rating = rating;
         this.date = date;
         this.author = author;
-        this.comment = comments;
+        this.comment = comment;
         this.tags = tags;
     }
 
-    public String getBeachName () {
+    // Getters and setters
+    public String getBeachName() {
         return beachName;
+    }
+
+    public void setBeachName(String beachName) {
+        this.beachName = beachName;
     }
 
     public Double getRating() {
         return rating;
     }
 
-    public Date getDate() {
-        return date;
-    }
-
-    public User getAuthor() {
-        return author;
-    }
-
-    public String getComments() {
-        return comment;
-    }
-
     public void setRating(Double rating) {
         this.rating = rating;
     }
 
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
     public void setComment(String comment) {
         this.comment = comment;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
     }
 }
